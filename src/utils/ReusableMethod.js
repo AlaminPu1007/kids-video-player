13af8995b01e1a3c92c4bfd66c866407aa4f9987
alaminpu1007 <alamin66.sit@gmail.com>
2023-07-17 22:15:49 +0600

    helper method is create to get unique id

--- src/utils/ReusableMethod.js
+++ src/utils/ReusableMethod.js
@@ -17,3 +17,34 @@ export const calculatePlayerHeight = () => {
     const playerHeight = screenWidth / aspectRatio;
     return playerHeight;
 };
+
+/**
+ * description :- This method help us to get an unique id
+ * @created_by :- {ALAMIN}
+ * @created_at :- 17/07/2023 22:14:53
+ */
+export const generateUUID = () => {
+    // Public Domain/MIT
+    let d = new Date().getTime(); //Timestamp
+    let d2 =
+        (typeof performance !== 'undefined' &&
+            performance.now &&
+            performance.now() * 1000) ||
+        0; //Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+        /[xy]/g,
+        function (c) {
+            let r = Math.random() * 16; //random number between 0 and 16
+            if (d > 0) {
+                //Use timestamp until depleted
+                r = (d + r) % 16 | 0;
+                d = Math.floor(d / 16);
+            } else {
+                //Use microseconds since page-load if supported
+                r = (d2 + r) % 16 | 0;
+                d2 = Math.floor(d2 / 16);
+            }
+            return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+        },
+    );
+};
